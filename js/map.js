19d629a39648fafd848650ddae2cf5fa8cdbe122
rrlara <rrlara@gmail.com>
2015-05-04 17:33:46 -0700

    active moment

--- js/map.js
+++ js/map.js
@@ -9,7 +9,8 @@ app.controller('MapCtrl', ['$scope','$rootScope', function($scope, $rootScope){
     if (map != undefined) { map.remove(); }
 
     var map = L.map('map', {
-        invalidateSize: true
+        invalidateSize: true,
+        zoomControl:false
     }).setView([47.6033068, -122.3077322], 10);
     var basemapUrl = 'http://{s}.tiles.mapbox.com/v3/spatialdev.map-4o51gab2/{z}/{x}/{y}.png';
     basemapLayer = L.tileLayer(basemapUrl,{
