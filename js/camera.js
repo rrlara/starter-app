ce649ac5031b424ad2f25407a8b646e6f49c6427
rrlara <rrlara@gmail.com>
2015-05-06 16:40:09 -0700

    phone camera

--- js/camera.js
+++ js/camera.js
@@ -9,8 +9,14 @@ app.controller('CameraCtrl', function($scope, $rootScope, $mdDialog, $timeout){
     $scope.thumbnail = {
         dataUrl: 'adsfas'
     };
+
+    $scope.comment = {};
+    $scope.imageFile = {};
+
     $scope.fileReaderSupported = window.FileReader != null;
     $scope.photoChanged = function(files){
+        console.log(files);
+        $scope.imageFile = files[0];
         if (files != null) {
             var file = files[0];
             if ($scope.fileReaderSupported && file.type.indexOf('image') > -1) {
@@ -32,7 +38,7 @@ app.controller('CameraCtrl', function($scope, $rootScope, $mdDialog, $timeout){
     $scope.showCameraPreview = function(ev) {
         $mdDialog.show({
             controller: DialogController,
-            template: '<md-dialog aria-label="Mango (Fruit)"> <md-content class="md-padding"> <img height="200" width="250" ng-src="' + $scope.thumbnail.dataUrl + '"/><form name="userForm"> <md-input-container flex> <label>Biography</label> <textarea ng-model="user.biography" columns="1" md-maxlength="150"></textarea> </md-input-container> </form> </md-content> <div class="md-actions" layout="row"> <span flex></span> <md-button ng-click="answer(\'not useful\')"> Cancel </md-button> <md-button ng-click="answer(\'useful\')" class="md-primary"> Save </md-button> </div></md-dialog>',
+            template: '<md-dialog aria-label="Mango (Fruit)"> <md-content class="md-padding"> <img height="200" width="200" ng-src="' + $scope.thumbnail.dataUrl + '"/><form name="userForm"> <md-input-container flex> <label>Biography</label> <textarea ng-model="comment" columns="1" md-maxlength="150"></textarea> </md-input-container> </form> </md-content> <div class="md-actions" layout="row"> <span flex></span> <md-button ng-click="answer(\'not useful\')"> Cancel </md-button> <md-button ng-click="answer()" class="md-primary"> Save </md-button> </div></md-dialog>',
             targetEvent: ev
         })
             .then(function(answer) {
@@ -50,9 +56,58 @@ app.controller('CameraCtrl', function($scope, $rootScope, $mdDialog, $timeout){
             $mdDialog.cancel();
         };
         $scope.answer = function(answer) {
+            uploadData($scope.comment);
             $mdDialog.hide(answer);
+            console.log($scope.comment);
+
         };
     };
+
+    function uploadData (comment){
+        var userDataEntry = {};
+            //create photo 1 as a parse file
+            var parseFile1 = null;
+            if ($scope.imageFile) {
+                parseFile1 = new Parse.File("photo.jpg", $scope.imageFile);
+            }
+
+
+
+            userDataEntry.altitude = '';
+            userDataEntry.latitude = '';
+            userDataEntry.longitude = '';
+            userDataEntry.comment = comment;
+            userDataEntry.imageFile = parseFile1;
+            userDataEntry.trueHeading = '';
+            userDataEntry.thumbnail = parseFile1;
+
+        saveToParse(userDataEntry);
+    }
+
+    function saveToParse(obj){
+        var GameScore = Parse.Object.extend("washingtondc");
+        var gameScore = new GameScore();
+
+        gameScore.set("altitude", obj.altitude);
+        gameScore.set("latitude", obj.latitude);
+        gameScore.set("longitude", obj.longitude);
+        gameScore.set("comment", obj.comment);
+        gameScore.set("imageFile", obj.imageFile);
+        gameScore.set("trueHeading", obj.trueHeading);
+        gameScore.set("thumbnail", obj.thumbnail);
+
+        gameScore.save(null, {
+            success: function(gameScore) {
+                // Execute any logic that should take place after the object is saved.
+                alert('New object created with objectId: ' + gameScore.id);
+            },
+            error: function(gameScore, error) {
+                // Execute any logic that should take place if the save fails.
+                // error is a Parse.Error with an error code and message.
+                alert('Failed to create new object, with error code: ' + error.message);
+            }
+        });
+    }
 
 
 });
